6a38fcffb1eda71f736ab1f6455ae35daeb90b64
adam <adamschunk@gmail.com>
2017-05-30 13:50:35 -0400

    added support to graph results

--- generate_network.py
+++ generate_network.py
@@ -10,23 +10,42 @@
 
 import sys
 import json
+import networkx as nx
+import matplotlib.pyplot as plt
 from twitter_tools import Twitter_Tools
 
+
+	
+	
+def gen_network_from_tweets(tweet_file):
+	input = open(tweet_file, "r")
+	tweets = json.load(input)
+	nodes = set()
+	graph = nx.Graph()
+	edges = []
+	for tweet in tweets:
+		retweeter = tweet["user"]["screen_name"]
+		retweeted = tweet["retweeted_status"]["retweeted_from"]
+		nodes.add(retweeter)
+		nodes.add(retweeted)
+		edges.append([retweeted, retweeter])
+	
+	for node in nodes:
+		graph.add_node(node)
+		
+	for edge in edges:
+		graph.add_edge(edge[0],edge[1])
+	
+	print("drawing graph")
+	nx.draw(graph)
+	plt.show()
+	#use networkx for all of the netwrork stuff
+	
+	
 if __name__ == "__main__":
 	tweet_thresh = int(sys.argv[2])
 	query = sys.argv[1]
 	tt = Twitter_Tools()
 	tweet = tt.find_tweet_with_num_retweets(query, tweet_thresh)
 	tt.trace_retweets(tweet)
-	
-	
-def gen_network_from_tweets(tweet_file):
-	input = open(tweet_file, "r")
-	tweets = json.loads(output)
-	users = set()
-	for tweet in tweets:
-		users.add(tweet["user"]["screen_name"])
-		users.add(tweet["retweeted_status"]["retweeted_from"]
-		
-		#use networkx for all of the netwrork stuff
-	
+	gen_network_from_tweets("output.txt")
