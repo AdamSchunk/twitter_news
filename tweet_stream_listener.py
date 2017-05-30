f3556ed290eaecc3fb179ba9f80c00a1939ffcd0
adam <adamschunk@gmail.com>
2017-05-30 14:16:47 -0400

    now only finds tweets within +25% of the given threshold
    This will probably be removed, but its good for a demo

--- tweet_stream_listener.py
+++ tweet_stream_listener.py
@@ -18,7 +18,7 @@ class Tweet_Stream_Listener(tweepy.StreamListener):
 	def on_status(self, status):
 		filtered_tweet = self.tweet_parser.parse_tweet(status)
 		num_retweets = int(filtered_tweet["retweeted_status"]["retweet_count"])
-		if (self.mode == 0 and num_retweets >= self.tweet_thresh):
+		if (self.mode == 0 and num_retweets >= self.tweet_thresh and num_retweets <= self.tweet_thresh * 1.25):
 			self.data.append(filtered_tweet)
 			#print(filtered_tweet)
 			return False
