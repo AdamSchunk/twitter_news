beb04f91c97f153fb8f3f849709ece2614ab971d
adam <adamschunk@gmail.com>
2017-06-23 16:30:21 -0400

    add case where user not authorized to be viewed

--- twitter_tools.py
+++ twitter_tools.py
@@ -1,3 +1,4 @@
+import os
 import csv
 import sys
 import time
@@ -35,12 +36,16 @@ class Twitter_Tools(object):
 				res = twitter_call(*twitter_args)
 			else:
 				res = twitter_call(twitter_args)
-		except tweepy.TweepError:    
+		except tweepy.RateLimitError as e:
 			print("******sleeping until more requests available (16 min)*******")
 			print("current time: " + datetime.now().strftime('%Y-%m-%d_%H:%M:%S'))
 			time.sleep(60*16)
 			print("resuming")
 			return self.timeout_safe_call(twitter_call, twitter_args)
+			
+		except tweepy.TweepError as e:
+			print(e)
+			return -2
 		except StopIteration:
 			return -1
 			
@@ -55,14 +60,23 @@ class Twitter_Tools(object):
 			
 	def save_user_is_following(self, user_name):
 		indexed_users = [f for f in listdir("users") if isfile(join("users", f))]
-		if(user_name not in indexed_users):
-			output_file = open("users/" + str(user_name), "w")
+		if(str(user_name) not in indexed_users):
 			following_list = tweepy.Cursor(self.api.friends_ids, id=user_name, count=5000).items()
+			output_file = open("users/" + str(user_name), "w")
+			i = 0
 			while True:
 				user = self.timeout_safe_call(following_list.next)
 				if user == -1:
 					break
+				if user == -2:
+					print("can not access user")
+					output_file.close()
+					#os.remove("users/" + str(user_name))
+					break
 				output_file.write(str(user) + "\n")
+					
+		#else:
+			#print("user already indexed")
 		
 	def get_is_following_user(self, user_name):
 		print("Friends of: " + user_name)
@@ -80,24 +94,31 @@ class Twitter_Tools(object):
 		StreamListener = Tweet_Stream_Listener(query, 0, tweet_thresh)
 		Stream = tweepy.Stream(auth=self.api.auth, listener = StreamListener)
 		Stream.filter(track=[query], async=True)
+		Tweet = None
 		
-		while not StreamListener.get_data():
-			pass
+		while True:
 		
-		already_seen_tweets = [f for f in listdir("tweet_searches") if isfile(join("tweet_searches", f))]
-		tweet = StreamListener.get_data()[0]
-		
-		if tweet["retweeted_status"]["retweet_id"] not in already_seen_tweets:
-			output_file = "tweet_searches/" + str(tweet["retweeted_status"]["retweet_id"])
-			self.tweet_parser.save_tweets_json(tweet, output_file)
+			while not StreamListener.get_data():
+				pass
+			
+			already_seen_tweets = [f for f in listdir("tweet_searches") if isfile(join("tweet_searches", f))]
+			tweet = StreamListener.get_data()[0]
+			
+			print(already_seen_tweets)
+			
+			if str(tweet["retweeted_status"]["retweet_id"]) not in already_seen_tweets:
+				output_file = "tweet_searches/" + str(tweet["retweeted_status"]["retweet_id"])
+				self.tweet_parser.save_tweets_json(tweet, output_file)
+				break
 		
 		return tweet
 		
 	def trace_retweets(self, tweet):
-		print("finding past tweets")
+		print("finding past tweets with:")
+		print(tweet["text"])
 		tweets = self.search_past_tweets(tweet["text"])
 		print(len(tweets))
-		output_file = "tweet_search_results/" + datetime.now().strftime('%Y-%m-%d_%H-%M')
+		output_file = "tweet_search_results/" + datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 		self.tweet_parser.save_tweets_json(tweets, output_file)
 	
 	def init_stream_search(self, search_term):
@@ -106,11 +127,7 @@ class Twitter_Tools(object):
 	def search_past_tweets(self, seach_term): 
 		tweet_iterator = tweepy.Cursor(self.api.search, q=seach_term, lang="en").items()
 		filtered_tweets = []
-		i = 0
 		while True:
-			i += 1
-			if 1%100 == 0:
-				print( str(i) + " tweets found")
 			tweet = self.timeout_safe_call(tweet_iterator.next)
 			if tweet == -1:
 				break
